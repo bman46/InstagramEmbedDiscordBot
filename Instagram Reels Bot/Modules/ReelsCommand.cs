5065a4f1a38da98a4a0b7f600c99fd0175ce6eac
Brendan McShane <brendanmcs7@gmail.com>
2021-03-04 12:57:46 -0500

    Add support for normal instagram posts

--- Instagram Reels Bot/Modules/ReelsCommand.cs
+++ Instagram Reels Bot/Modules/ReelsCommand.cs
@@ -14,6 +14,11 @@ namespace Instagram_Reels_Bot.Modules
 {
     public class ReelsCommand : ModuleBase
     {
+        /// <summary>
+        /// Parse reel URL:
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         [Command("reel")]
         public async Task ReelParser([Remainder] string args = null)
         {
@@ -26,6 +31,32 @@ namespace Instagram_Reels_Bot.Modules
 
             //return result:
             await ReplyAsync("Video from " + Context.Message.Author.Mention + "'s reel: " + videourl);
+        }
+        /// <summary>
+        /// Parse an instagram post:
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        [Command("p")]
+        public async Task PostParser([Remainder] string args = null)
+        {
+            string url = "https://www.instagram.com/p/" + args.Replace(" ", "/");
+            //Parse for Opengraph Video url:
+            OpenGraph graph = OpenGraph.ParseUrl(url, "");
+            if (graph.Metadata["og:video"].Count > 0)
+            {
+                await ReplyAsync("Video from " + Context.Message.Author.Mention + "'s post: " + graph.Metadata["og:video"].First().Value);
+            }
+            else
+            {
+                var embed = new EmbedBuilder();
+                embed.Title = "Content from " + Context.Message.Author.Username + "'s post";
+                embed.Url = graph.Metadata["og:url"].First().Value;
+                embed.Description = graph.Metadata["og:title"].First().Value;
+                embed.ImageUrl = graph.Metadata["og:image"].First().Value;
+                embed.WithColor(new Color(131, 58, 180));
+                await ReplyAsync(null, false, embed.Build());
+            }
         }
     }
 }

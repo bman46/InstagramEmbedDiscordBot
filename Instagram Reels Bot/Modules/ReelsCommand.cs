bb3391d868173cdccc4b71fc97c26e4fc8e9e725
Brendan McShane <brendanmcs7@gmail.com>
2021-11-07 20:39:45 -0500

    Truncate ig post captions

--- Instagram Reels Bot/Modules/ReelsCommand.cs
+++ Instagram Reels Bot/Modules/ReelsCommand.cs
@@ -10,6 +10,10 @@ namespace Instagram_Reels_Bot.Modules
     public class ReelsCommand : ModuleBase
     {
         /// <summary>
+        /// Sets the message size for embeds.
+        /// </summary>
+        private static readonly int MAX_MSG_SIZE = 40;
+        /// <summary>
         /// Parse reel URL:
         /// </summary>
         /// <param name="args"></param>
@@ -171,7 +175,7 @@ namespace Instagram_Reels_Bot.Modules
                 var embed = new EmbedBuilder();
                 embed.Title = "Content from " + Context.Message.Author.Username + "'s linked post";
                 embed.Url = url;
-                embed.Description = (media.Value.Caption!=null)?(media.Value.Caption.Text):("");
+                embed.Description = (media.Value.Caption!=null)?(Truncate(media.Value.Caption.Text, MAX_MSG_SIZE)):("");
                 embed.ImageUrl = media.Value.Images[0].Uri;
                 embed.WithColor(new Color(131, 58, 180));
                 await ReplyAsync(null, false, embed.Build());
@@ -268,6 +272,45 @@ namespace Instagram_Reels_Bot.Modules
                     //Default 8MB Upload Limit
                     return 8000000;
             }
+        }
+        /// <summary>
+        /// Cuts the size of the string down.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="length"></param>
+        /// <param name="atWord"></param>
+        /// <param name="addEllipsis"></param>
+        /// <returns></returns>
+        private static string Truncate(string s, int length, bool atWord = true, bool addEllipsis = true)
+        {
+            // Return if the string is less than or equal to the truncation length
+            if (s == null || s.Length <= length)
+                return s;
+
+            // Do a simple tuncation at the desired length
+            string s2 = s.Substring(0, length);
+
+            // Truncate the string at the word
+            if (atWord)
+            {
+                // List of characters that denote the start or a new word (add to or remove more as necessary)
+                List<char> alternativeCutOffs = new List<char>() { ' ', ',', '.', '?', '/', ':', ';', '\'', '\"', '\'', '-' };
+
+                // Get the index of the last space in the truncated string
+                int lastSpace = s2.LastIndexOf(' ');
+
+                // If the last space index isn't -1 and also the next character in the original
+                // string isn't contained in the alternativeCutOffs List (which means the previous
+                // truncation actually truncated at the end of a word),then shorten string to the last space
+                if (lastSpace != -1 && (s.Length >= length + 1 && !alternativeCutOffs.Contains(s.ToCharArray()[length])))
+                    s2 = s2.Remove(lastSpace);
+            }
+
+            // Add Ellipsis if desired
+            if (addEllipsis)
+                s2 += "...";
+
+            return s2;
         }
     }
 }

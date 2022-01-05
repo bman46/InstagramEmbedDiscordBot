6ecc930db64c7c48c494a4b3e3979714786d1543
Brendan McShane <brendanmcs7@gmail.com>
2022-01-05 18:32:46 -0500

    Update

--- Instagram Reels Bot/Services/Subscriptions.cs
+++ Instagram Reels Bot/Services/Subscriptions.cs
@@ -11,27 +11,27 @@ namespace Instagram_Reels_Bot.Services
     /// Allows users to get the lastest posts from an account
     /// </summary>
 	public class Subscriptions
-	{
-		private readonly IConfiguration config;
-		private readonly DiscordShardedClient client;
+    {
+        private readonly IConfiguration config;
+        private readonly DiscordShardedClient client;
         private Timer UpdateTimer;
 
-		public Subscriptions(IServiceProvider services)
-		{
+        public Subscriptions(IServiceProvider services)
+        {
             // Dependancy injection:
             config = services.GetRequiredService<IConfiguration>();
             client = services.GetRequiredService<DiscordShardedClient>();
         }
-		/// <summary>
+        /// <summary>
         /// Starts the subscription tasks.
         /// </summary>
         /// <returns></returns>
-		public async Task InitializeAsync()
+        public async Task InitializeAsync()
         {
-            if (config["AllowSubscriptions"].ToLower()=="true")
+            if (config["AllowSubscriptions"].ToLower() == "true")
             {
                 // TODO: Initialization.
-                UpdateTimer = new Timer(3600000.0*double.Parse(config["HoursToCheckForNewContent"])); //one hour in milliseconds
+                UpdateTimer = new Timer(3600000.0 * double.Parse(config["HoursToCheckForNewContent"])); //one hour in milliseconds
                 UpdateTimer.Elapsed += new ElapsedEventHandler(GetLatestsPosts);
                 UpdateTimer.Start();
             }
@@ -42,8 +42,7 @@ namespace Instagram_Reels_Bot.Services
         }
         async void GetLatestsPosts(object sender, System.Timers.ElapsedEventArgs e)
         {
-           
-        }
-	}
-}
 
+        }
+    }
+}

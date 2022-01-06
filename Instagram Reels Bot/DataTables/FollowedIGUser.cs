585ba410395dc53a2bae55b2558caa2e042ce1b0
Brendan McShane <brendanmcs7@gmail.com>
2022-01-05 22:20:56 -0500

    Add followediguser class

---
+++ Instagram Reels Bot/DataTables/FollowedIGUser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Instagram_Reels_Bot.DataTables
+{
+	public class FollowedIGUser
+	{
+		public FollowedIGUser()
+		{
+            LastPostDate = DateTime.Now;
+            LastCheckTime = DateTime.Now;
+        }
+
+        [JsonProperty(PropertyName = "id")]
+        public string InstagramID { get; set; }
+        public DateTime LastPostDate { get; set; }
+        public DateTime LastCheckTime { get; set; }
+        public List<RespondChannel> SubscribedChannels { get; set; }
+        
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+    /// <summary>
+    /// The channel and guild to respond to new posts with.
+    /// TODO: ensure that this always exists (deleted channels must unsubscribe)
+    /// </summary>
+    public class RespondChannel
+    {
+        public RespondChannel(ulong guildID, ulong ChannelID)
+        {
+            this.GuildID = guildID;
+            this.ChannelID = ChannelID;
+        }
+        public ulong GuildID { get; set; }
+        public ulong ChannelID { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
+

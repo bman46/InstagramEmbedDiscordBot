c2a40abb5f6d5b89ecd13b8a3bfecd51fe431efa
Brendan McShane <brendanmcs7@gmail.com>
2022-01-12 14:55:20 -0500

    Add email and SMS tools

---
+++ Instagram Reels Bot/Helpers/Communications/EmailTools.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Instagram_Reels_Bot.Helpers
+{
+	public class EmailTools
+	{
+		public EmailTools()
+		{
+
+		}
+	}
+}
+
---
+++ Instagram Reels Bot/Helpers/Communications/SMSTools.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Instagram_Reels_Bot.Helpers
+{
+	public class SMSTools
+	{
+		public SMSTools()
+		{
+		}
+	}
+}
+

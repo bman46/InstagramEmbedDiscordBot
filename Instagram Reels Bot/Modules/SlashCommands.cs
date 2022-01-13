dd3f72416912b5c81147618b59d9e6e7397fb871
Brendan McShane <brendanmcs7@gmail.com>
2022-01-13 12:19:31 -0500

    Update help command to include permissions

--- Instagram Reels Bot/Modules/SlashCommands.cs
+++ Instagram Reels Bot/Modules/SlashCommands.cs
@@ -113,6 +113,11 @@ namespace Instagram_Reels_Bot.Modules
 			embed.AddField("Embedding Posts", "To embed the contents of an Instagram url, simply paste the link into the chat and the bot will do the rest (as long as it has permission to).\nYou can also use the `/link` along with a URL.\nFor posts with multiple slides, use the `/link` command along with the optional `Index:` parameter to select the specific slide.");
 			embed.AddField("Subscriptions", "Note: The subscriptions module is currently under beta testing.\nTo subscribe to an account, use `/subscribe` and the users Instagram account to get new posts from that account delivered to the channel where the command is executed.\nTo unsubscribe from an account, use `/unsubscribe` and the username of the Instagram account in the channel that is subscribed to the account. You can also use `/unsubscribeall` to unsubscribe from all Instagram accounts.\nUse `/subscribed` to list all of the Instagram accounts that the guild is subscribed to.");
 			embed.AddField("Roles", "Only users with the role `InstagramBotSubscribe` (case sensitive) or guild administrator permission are allowed to unsubscribe and subscribe to accounts.");
+			embed.AddField("Permissions", "The following channel permissions are required for the bot's operation:\n" +
+				"- `Send Messages`\n" +
+				"- `View Channel`\n" +
+                "- `Attach Files`\n" +
+                "- `Manage Messages` (optional-used to remove duplicate embeds)");
 			embed.WithColor(new Color(131, 58, 180));
 			await RespondAsync(embed: embed.Build(), ephemeral: false);
 		}

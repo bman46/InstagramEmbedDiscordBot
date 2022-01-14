8a98e3566921be7853fd0741f7d0a33d116e0daf
Brendan McShane <brendanmcs7@gmail.com>
2022-01-14 14:01:27 -0500

    Bump default truncation limit to 200 chars

--- Instagram Reels Bot/Helpers/DiscordTools.cs
+++ Instagram Reels Bot/Helpers/DiscordTools.cs
@@ -19,7 +19,7 @@ namespace Instagram_Reels_Bot.Helpers
         /// <param name="atWord">Try to cutoff at the end or start of a word instead of the middle.</param>
         /// <param name="addEllipsis">Add ... to the end of a truncated string.</param>
         /// <returns></returns>
-        public static string Truncate(string s, int length = 100, bool atWord = true, bool addEllipsis = true)
+        public static string Truncate(string s, int length = 200, bool atWord = true, bool addEllipsis = true)
         {
             // Dont process null values:
             if (s == null)

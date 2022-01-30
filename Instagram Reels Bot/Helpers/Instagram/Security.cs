31f3fae457cda04293872ddc889dec3ad11eb16d
Brendan McShane <brendanmcs7@gmail.com>
2022-01-30 15:26:39 -0500

    Security class

---
+++ Instagram Reels Bot/Helpers/Instagram/Security.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using OtpNet;
+
+namespace Instagram_Reels_Bot.Helpers.Instagram
+{
+	public class Security
+	{
+		/// <summary>
+        /// Gets a 2FA code
+        /// </summary>
+        /// <param name="secret">The secret for generating the OTP</param>
+        /// <returns>6 digit OTP</returns>
+		public static string GetTwoFactorAuthCode(string secret)
+		{
+			//Convert secret:
+			var bytes = Base32Encoding.ToBytes(secret);
+			var totp = new Totp(bytes);
+
+			if (totp.RemainingSeconds() > 1)
+			{
+				return totp.ComputeTotp();
+			}
+			else
+			{
+				//Wait for next code if the current code is about to expire:
+				Thread.Sleep(totp.RemainingSeconds() * 1000 + 100);
+				return totp.ComputeTotp();
+			}
+		}
+	}
+}
+

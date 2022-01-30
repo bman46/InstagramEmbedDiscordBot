cba4ef58e74d54ff85dd8c8633558d1d79ee9b81
Brendan McShane <brendanmcs7@gmail.com>
2022-01-30 15:26:30 -0500

    Add security class

--- Instagram Reels Bot/Helpers/Instagram/InstagramProcessor.cs
+++ Instagram Reels Bot/Helpers/Instagram/InstagramProcessor.cs
@@ -19,6 +19,17 @@ namespace Instagram_Reels_Bot.Helpers
 {
     public class InstagramProcessor
 	{
-        
+        /// <summary>
+        /// The IG processor for the account:
+        /// </summary>
+        public InstagramApiSharp.API.IInstaApi instaApi;
+
+        /// <summary>
+        /// Class responsible for getting an account to use the IG Processor with
+        /// </summary>
+        public class AccountFinder
+        {
+            
+        }
     }
 }

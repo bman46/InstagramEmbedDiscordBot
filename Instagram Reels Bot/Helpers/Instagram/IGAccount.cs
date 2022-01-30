44f6b904a1b441ad984f2014e42f0180aea2d70e
Brendan McShane <brendanmcs7@gmail.com>
2022-01-30 16:02:32 -0500

    IG Account function

--- Instagram Reels Bot/Helpers/Instagram/IGAccount.cs
+++ Instagram Reels Bot/Helpers/Instagram/IGAccount.cs
@@ -6,33 +6,16 @@ namespace Instagram_Reels_Bot.Helpers.Instagram
 {
 	public class IGAccount : UserSessionData
 	{
+		/// <summary>
+        /// Default class constructor
+        /// </summary>
 		public IGAccount() { }
-		/// <summary>
-		/// Account information without 2FA code.
-		/// </summary>
-		/// <param name="username">The account username</param>
-		/// <param name="password">The account password</param>
-		public IGAccount(string username, string password)
-		{
-			this.UserName = username;
-			this.Password = password;
-		}
-		/// <summary>
-		///  Account information with 2FA code.
-		/// </summary>
-		/// <param name="username">The account username</param>
-		/// <param name="password">The account password</param>
-		/// <param name="OTPSecret">The 2FA OTP secret code</param>
-		public IGAccount(string username, string password, string OTPSecret)
-		{
-			this.UserName = username;
-			this.Password = password;
-			this.OTPSecret = OTPSecret;
-		}
+
 		/// <summary>
 		/// The 2FA secret code for generating OTPs
 		/// </summary>
 		public string OTPSecret { get; set; }
+
 		/// <summary>
 		/// Set to true if login failed.
 		/// Defaults to false.
@@ -43,12 +26,12 @@ namespace Instagram_Reels_Bot.Helpers.Instagram
         /// <summary>
         /// List of times that this account should be used at
         /// </summary>
-        public List<OperatingTimes> UsageTimes = new List<OperatingTimes>();
+        public List<OperatingTime> UsageTimes = new List<OperatingTime>();
 
 		/// <summary>
         /// A single time for the account to be used at
         /// </summary>
-		public class OperatingTimes
+		public class OperatingTime
 		{
 			/// <summary>
 			/// Time to start using this account
@@ -58,6 +41,15 @@ namespace Instagram_Reels_Bot.Helpers.Instagram
 			/// Time to stop using the account
 			/// </summary>
 			public TimeOnly EndTime;
+
+			/// <summary>
+            /// Check to see if the time is valid
+            /// </summary>
+            /// <returns></returns>
+			public bool BetweenStartAndEnd(TimeOnly checkTime)
+            {
+				return checkTime >= StartTime && checkTime <= EndTime;
+            }
 		}
         #endregion
 
--- Instagram Reels Bot/Helpers/Instagram/InstagramProcessor.cs
+++ Instagram Reels Bot/Helpers/Instagram/InstagramProcessor.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using OtpNet;
 using System.Linq;
 using System.Net;
+using Instagram_Reels_Bot.Helpers.Instagram;
 
 namespace Instagram_Reels_Bot.Helpers
 {
@@ -27,9 +28,66 @@ namespace Instagram_Reels_Bot.Helpers
         /// <summary>
         /// Class responsible for getting an account to use the IG Processor with
         /// </summary>
-        public class AccountFinder
+        public static class AccountFinder
         {
-            
+            /// <summary>
+            /// List of accounts
+            /// </summary>
+            public static List<IGAccount> Accounts = new List<IGAccount>();
+
+            /// <summary>
+            /// Loads accounts from the Config file:
+            /// </summary>
+            public static void LoadAccounts()
+            {
+                // create the configuration
+                var _builder = new ConfigurationBuilder()
+                    .SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile(path: "config.json");
+
+                // build the configuration and assign to _config          
+                var config = _builder.Build();
+
+                //Add accounts to the array:
+                List<IGAccount> creds = config.GetSection("IGAccounts").Get<List<IGAccount>>();
+                Accounts = creds;
+            }
+            /// <summary>
+            /// Gets a valid IG account.
+            /// </summary>
+            /// <returns>An IG Account</returns>
+            /// <exception cref="InvalidDataException">No accounts avaliable</exception>
+            public static IGAccount GetIGAccount()
+            {
+                //Randomize the accounts
+                Random rand = new Random();
+                var shuffledAccounts = Accounts.OrderBy(x => rand.Next()).ToList();
+
+                //Find a valid account
+                foreach (IGAccount cred in shuffledAccounts)
+                {
+                    TimeOnly timeNow = TimeOnly.FromDateTime(DateTime.Now);
+                    if (!cred.FailedLogin)
+                    {
+                        if (cred.UsageTimes.Count > 0)
+                        {
+                            foreach (IGAccount.OperatingTime time in cred.UsageTimes)
+                            {
+                                if (time.BetweenStartAndEnd(timeNow))
+                                {
+                                    return cred;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Warning: No time set on account " + cred.UserName);
+                            return cred;
+                        }
+                    }
+                }
+                throw new InvalidDataException("No avaliable accounts.");
+            }
         }
     }
 }

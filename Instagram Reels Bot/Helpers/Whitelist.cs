0398b23f668b0beb6c9b1962da6edd3a63cb80e5
Brendan McShane <brendanmcs7@gmail.com>
2022-02-08 20:58:06 -0500

    Add whitelist class

---
+++ Instagram Reels Bot/Helpers/Whitelist.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Instagram_Reels_Bot.Helpers
+{
+	public class Whitelist
+	{
+		public static List<ulong> WhitelistedServers = new List<ulong>();
+		public static bool ListSet = false;
+		public static bool WhitelistEnabled = false;
+
+		public static bool IsServerOnList(ulong serverID)
+        {
+			// Load the list in:
+            if (!ListSet)
+            {
+				SetWhitelistEnabledBool();
+                if (WhitelistEnabled)
+                {
+					LoadList();
+                }
+                else
+                {
+					ListSet = true;
+                }
+            }
+			else if (WhitelistedServers.Count==0)
+            {
+				// No whitelist enabled:
+				return true;
+            }
+
+			// Check to see if server is listed:
+			return WhitelistedServers.Contains(serverID);
+        }
+
+		/// <summary>
+        /// Check to see if whitelist is enabled in the config.
+        /// </summary>
+		private static void SetWhitelistEnabledBool()
+        {
+			var _builder = new ConfigurationBuilder()
+				.SetBasePath(AppContext.BaseDirectory)
+				.AddJsonFile(path: "config.json");
+
+			// build the configuration and assign to _config          
+			var config = _builder.Build();
+
+			WhitelistEnabled = config["Whitelist"].ToLower().Equals("true");
+		}
+
+		/// <summary>
+        /// Load the list of whitelisted servers.
+        /// </summary>
+		private static void LoadList()
+        {
+			// Set the bool to true:
+			ListSet = true;
+
+			// Load in the servers:
+			try
+			{
+				string[] lines = System.IO.File.ReadAllLines(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "whitelist.txt");
+				foreach(string line in lines)
+                {
+                    if (!line.StartsWith('#'))
+                    {
+                        try
+                        {
+							WhitelistedServers.Add(ulong.Parse(line));
+                        }
+						catch(Exception e)
+                        {
+							Console.WriteLine("Error loading whitelist line. Error: " + e);
+                        }
+                    }
+                }
+
+			}
+			catch(Exception e)
+            {
+				Console.WriteLine("Error loading whitelist. Error: " + e);
+            }
+		}
+	}
+}
+

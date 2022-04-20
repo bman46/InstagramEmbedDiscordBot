362e10ca8d2776a91efe2aa1a6c64ac722dd1836
Brendan McShane <brendanmcs7@gmail.com>
2022-04-20 00:49:08 -0400

    Remove excess using

--- Instagram Reels Bot/Modules/ComponentRespond.cs
+++ Instagram Reels Bot/Modules/ComponentRespond.cs
@@ -4,7 +4,6 @@ using Discord.WebSocket;
 using Instagram_Reels_Bot.Helpers;
 using Instagram_Reels_Bot.Services;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Instagram_Reels_Bot.Modules
